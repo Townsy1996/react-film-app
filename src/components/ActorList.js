efd6c90e2d33bfcfe96ed6b896a3fbbfb96dfbcc
corey <corey_townsend@outlook.com>
2024-02-27 13:33:57 +0000

    create actorList component, include in starlist

---
+++ src/components/ActorList.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function ActorList() {
+    const [actors, setActors] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchActors = async () => {
+            try {
+                const response = await axios.get('http://16.171.0.136:8080/actor/getAll');
+                setActors(response.data);
+                setLoading(false);
+            } catch (error) {
+                setError(error.message);
+                setLoading(false);
+            }
+        };
+
+        fetchActors();
+    }, []);
+
+    return (
+        <div id="actor-list-container">
+            {loading ? (
+                <div id="loading-message">Loading...</div>
+            ) : error ? (
+                <div id="error-message">Error: {error}</div>
+            ) : (
+                <div>
+                    <h1 id="star-list-heading">Star List</h1>
+                    <ul id="actor-list">
+                        {actors.map(actor => (
+                            <li key={actor.id}>{actor.firstName} {actor.lastName}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default ActorList;
--- src/pages/StarList.js
+++ src/pages/StarList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import ActorList from '../components/ActorList';
 
-function Starlist (){
-
+function Starlist() {
     return (
         <div>
             <Header />
-          
+            <ActorList />
+            
         </div>
     );
 }
