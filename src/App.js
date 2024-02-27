fde15ca92af2a405c3b843ea3c2aaaac10f6bbd2
corey <corey_townsend@outlook.com>
2024-02-27 11:52:18 +0000

    create starList and findafilm pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';  
+import Findafilm from './pages/Findafilm';
+import Starlist from './pages/StarList';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/findafilm' element={<Findafilm />} />
+        <Route path='/starlist' element={<Starlist />} />
         
       </Routes>
     </Router>
---
+++ src/pages/Findafilm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Findafilm (){
+
+    return (
+        <div>
+            <Header />
+          
+        </div>
+    );
+}
+
+export default Findafilm;
