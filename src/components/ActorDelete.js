1f7341d204abe9af3b07f55ef049894fc4d4ab73
corey <corey_townsend@outlook.com>
2024-02-28 10:34:20 +0000

    added delete conformation box, updated styles

--- src/components/ActorDelete.js
+++ src/components/ActorDelete.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function ActorDelete({ actorId, onDelete }) {
+    const [showConfirmation, setShowConfirmation] = useState(false);
+
     const handleDelete = async () => {
+        setShowConfirmation(true);
+    };
+
+    const confirmDelete = async () => {
         try {
             await axios.delete(`http://16.171.0.136:8080/actor/delete/${actorId}`);
-            onDelete(actorId); 
+            onDelete(actorId);
+            setShowConfirmation(false);
         } catch (error) {
-           
             console.error('Error deleting actor:', error);
         }
     };
 
+    const cancelDelete = () => {
+        setShowConfirmation(false);
+    };
+
+    /* Delete actor button, after click conformation window appears */
+
     return (
-        <button onClick={handleDelete}>Delete</button>
+        <>
+
+
+            <button id='delete-button' onClick={handleDelete}>Delete Actor</button>
+            {showConfirmation && (
+                <div className="confirmation-overlay">
+                    <div className="confirmation-dialog">
+                        <p className="confirmation-message">Are you sure you want to delete?</p>
+                        <div className="confirmation-buttons">
+                            <button onClick={confirmDelete}>Yes, I am sure</button>
+                            <button onClick={cancelDelete}>No, cancel</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
 
